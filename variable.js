697bc332ccdd9fc59cfa90342302b76280cc944a
netramorjariya <118957750+netramorjariya@users.noreply.github.com>
2024-11-11 15:04:00 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,60 @@
+//Arithmetic operator--> +,-,*,/,%,**,++,--
+
+//---------------------------------------1)Nothing method to declar js variable
+a=10
+b=20
+//Addition os two numbers
+add=a+b
+document.write('Addition is '+add)
+
+//Subtraction of two numbers
+sub= b-a
+document.write('<br> substraction is '+sub)
+
+//Multiplication of two numbers
+mult= a*b
+document.write('<br> substraction is '+mult)
+
+//division of two numbers
+mult= b/a
+document.write('<br> substraction is '+div)
+
+//Modulus operator--> % it represent the reminder
+document.write('<br> Modulus is '+(7%3))
+
+//Double star use for square and cube
+a=5
+document.write('<br> Square is '+(a**2))
+document.write('<br> Cube is'+(a**3))
+
+//------------------------------------------------2) second method to create variable
+//i)var, ii)let, iii)const variable..
+
+//1) var key word..
+//it alloves you to redeclerection and reassign of same variable
+/*var a=25
+document.write('<br> A is '+a)*/
+
+var a="Hello js"
+document.write('<br> A is '+a)
+
+//2) Const key word..
+//it not allowes you to redeclerection and reassign of same variable
+
+const pi=3.14
+document.write('<br> pi is '+pi)
+
+//const pi=200
+//document.write('<br> pi is '+pi)
+
+
+
+//3) let key word..
+//It not allowes you to redeclerection But allows you to reassign of same variable
+let name = "Test"
+document.write('<br> Name is '+name)
+
+name='abc'
+document.write('<br> Name is '+name);
+
+
